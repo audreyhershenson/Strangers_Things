29413060fb13af27bb1441e6e4133f830a7a27ed
Audrey <audreyhershenson@gmail.com>
2022-07-07 17:56:07 -0500

    trying to get token

--- src/api/index.js
+++ src/api/index.js
@@ -6,37 +6,42 @@ const COHORT_NAME = '2206-FTB-ET-WEB-FT'
 
 
 export async function getAllPosts() {
-    try {
-      const response = await fetch(`${BASE}/posts`);
-      const data = await response.json();
-      return data;
-    } 
-    catch (error) {
-      console.error;
-    }
+  try {
+    const response = await fetch(`${BASE}/posts`);
+    const data = await response.json();
+    return data;
+  }
+  catch (error) {
+    console.error;
+  }
 }
 
 
 export async function registerUser(username, password) {
   console.log('Lets get the event inputs', event.target)
-  console.log(`${BASE_URL}${COHORT_NAME}/users/register`) 
-  const response = await 
-  fetch(`${BASE_URL}${COHORT_NAME}/users/register`,
-    {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        user: {
-          username: username,
-          password: password
-        }})
-    })
-    console.log(response, "response from user")
-    const result = await response.json()
-  console.log (result)
-    return result
+  console.log(`${BASE_URL}${COHORT_NAME}/users/register`)
+  const response = await
+    fetch(`${BASE_URL}${COHORT_NAME}/users/register`,
+      {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          user: {
+            username: username,
+            password: password
+          }
+        })
+      }).then(response => response.json())
+      .then(result => {
+        const token = result.data.token;
+        console.log(token);
+        return token;
+      })
+      .catch(console.error);
+
+
   // try {
   //   const response = await fetch(`${BASE}/users/register`);
   //   const data = await response.json();
@@ -47,14 +52,35 @@ export async function registerUser(username, password) {
   // }
 }
 
-// export async function Login() {
-//   try {
-//     const response = await fetch(`${BASE}/users/login`);
-//     const data = await response.json();
-//     return data;
-//   } 
-//   catch (error) {
-//     console.error;
-//   }
+// export async function loginUser(username, password) {
+//   console.log('Lets get the event inputs', event.target)
+//   console.log(`${BASE_URL}${COHORT_NAME}/users/login`)
+//   const response = await
+//     fetch('https://strangers-things.herokuapp.com/api/COHORT-NAME/users/login', {
+//       method: "POST",
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({
+//         user: {
+//           username: username,
+//           password: password
+//         }
+//       })
+//     }).then(response => response.json())
+//       .then(result => {
+//         console.log(result);
+//       })
+//       .catch(console.error);
+//   //console.log(response, "response from user")
+//   const result = await response.json()
+//   return result
+//   // try {
+//   //   const response = await fetch(`${BASE}/users/register`);
+//   //   const data = await response.json();
+//   //   return data;
+//   // } 
+//   // catch (error) {
+//   //   console.error;
+//   // }
 // }
-
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,8 @@ import {
 } from '../auth';
 
 import {
-    registerUser
+    registerUser,
+    loginUser
 } from '../api';
 //move to index
 
@@ -19,21 +20,28 @@ const Header = ({
 const [username, setUsername] = useState('');
 const [password, setPassword] = useState('');
     //move to login
-    async function handleSubmit(event) {
+    async function handleRegister(event) {
         event.preventDefault()
         console.log('this is your event', event)
         const backFromAPI = await registerUser(username, password)
+        localStorage.setItem('token', token)
         // backFromAPI ? console.log('Success') : console.log('User already exists') 
         console.log(backFromAPI) 
     }
+
+    // async function handleLogin(event) {
+    //     event.preventDefault()
+    //     console.log('this is your event', event)
+    //     const backFromAPI = await loginUser(username, password)
+    //     // backFromAPI ? console.log('Success') : console.log('User already exists') 
+    //     console.log(backFromAPI) 
+    // }
     return (
         <header>
-        
-            <h1>Strangers' Things</h1>
-            <form  id = "register" onSubmit = {handleSubmit}>
+            <form id = "register" onSubmit = {handleRegister}>
                 <div className="nameEntry">
                     <label>Username:</label>
-                    <input type="text" id="inputUsername" name="username"  
+                    <input type="text" id="inputUsernameReg" name="username"  
                     value = {username}
                     onChange={(event) =>{setUsername(event.target.value)
                     console.log(username);}}
@@ -41,7 +49,7 @@ const [password, setPassword] = useState('');
                 </div>
                 <div className="passwordEntry">
                     <label>Password:</label>
-                    <input type="text" id="inputPassword" name="password" 
+                    <input type="text" id="inputPasswordReg" name="password" 
                     value = {password}
                     onChange={(event) =>{setPassword(event.target.value)
                     console.log(password);}}    
@@ -49,13 +57,30 @@ const [password, setPassword] = useState('');
                 </div>
                 <div className="passwordConfirm">
                     <label>Confirm password:</label>
-                    <input type="text" id="inputPasswordC" name="passwordC" required />
+                    <input type="text" id="inputPasswordCReg" name="passwordC" required />
                 </div>
                 <button type="submit" id = "registerButton">Register</button>
-                <button type="submit" id = "loginButton">Login</button>
-
             </form>
 
+            {/* <form id = "login" onSubmit = {handleLogin}>
+                <div className="nameEntry">
+                    <label>Username:</label>
+                    <input type="text" id="inputUsernameLog" name="username"  
+                    value = {username}
+                    onChange={(event) =>{setUsername(event.target.value)
+                    console.log(username);}}
+                    required />
+                </div>
+                <div className="passwordEntry">
+                    <label>Password:</label>
+                    <input type="text" id="inputPasswordLog" name="password" 
+                    value = {password}
+                    onChange={(event) =>{setPassword(event.target.value)
+                    console.log(password);}}    
+                    required />
+                </div>
+                <button type="submit" id = "loginButton">Login</button>
+            </form> */}
         </header>
     );
 }
