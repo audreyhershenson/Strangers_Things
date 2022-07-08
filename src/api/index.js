b821b54136cdf8aa2af44bddfebe47d0c9a259a9
Anna Mantula <ganna.mantula@gmail.com>
2022-07-08 10:26:33 -0500

    token in local storage

--- src/api/index.js
+++ src/api/index.js
@@ -1,56 +1,83 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE = 'https://strangers-things.herokuapp.com/api/2206-FTB-ET-WEB-FT-A'
-const BASE_URL = 'https://strangers-things.herokuapp.com/api/'
-const COHORT_NAME = '2206-FTB-ET-WEB-FT'
-
+const BASE = "https://strangers-things.herokuapp.com/api/2206-FTB-ET-WEB-FT-A";
+const BASE_URL = "https://strangers-things.herokuapp.com/api/";
+const COHORT_NAME = "2206-FTB-ET-WEB-FT";
 
 export async function getAllPosts() {
   try {
     const response = await fetch(`${BASE}/posts`);
     const data = await response.json();
     return data;
-  }
-  catch (error) {
+  } catch (error) {
     console.error;
   }
 }
 
-
 export async function registerUser(username, password) {
-  console.log('Lets get the event inputs', event.target)
-  console.log(`${BASE_URL}${COHORT_NAME}/users/register`)
-  const response = await
-    fetch(`${BASE_URL}${COHORT_NAME}/users/register`,
-      {
+  console.log("Lets get the event inputs", event.target);
+  console.log(`${BASE_URL}${COHORT_NAME}/users/register`);
+
+  try {
+    const response = await fetch(`${BASE_URL}${COHORT_NAME}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: username,
+          password: password,
+        },
+      }),
+    });
+    const result = await response.json();
+    console.log(result, "result")
+    const token = result.data.token;
+    console.log(token);
+    return token;
+  } catch (error) {
+    console.error(error);
+  }
+
+}
+  export async function loginUser(username, password) {
+    console.log("Lets get the event inputs", event.target);
+    console.log(`${BASE_URL}${COHORT_NAME}/users/login`);
+  
+    try {
+      const response = await fetch(`${BASE_URL}${COHORT_NAME}/users/login`, {
         method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           user: {
             username: username,
-            password: password
-          }
-        })
-      }).then(response => response.json())
-      .then(result => {
-        const token = result.data.token;
-        console.log(token);
-        return token;
-      })
-      .catch(console.error);
+            password: password,
+          },
+        }),
+      });
+      const result = await response.json();
+      console.log(result, "result")
+      const token = result.data.token;
+      console.log(token);
+      return token;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 
   // try {
   //   const response = await fetch(`${BASE}/users/register`);
   //   const data = await response.json();
   //   return data;
-  // } 
+  // }
   // catch (error) {
   //   console.error;
   // }
-}
+
 
 // export async function loginUser(username, password) {
 //   console.log('Lets get the event inputs', event.target)
@@ -79,7 +106,7 @@ export async function registerUser(username, password) {
 //   //   const response = await fetch(`${BASE}/users/register`);
 //   //   const data = await response.json();
 //   //   return data;
-//   // } 
+//   // }
 //   // catch (error) {
 //   //   console.error;
 //   // }
--- src/components/Header.js
+++ src/components/Header.js
@@ -24,18 +24,21 @@ const [password, setPassword] = useState('');
         event.preventDefault()
         console.log('this is your event', event)
         const backFromAPI = await registerUser(username, password)
-        localStorage.setItem('token', token)
+        console.log(backFromAPI)
+        localStorage.setItem('token', backFromAPI)
+        // const token = localStorage.getItem("token");
         // backFromAPI ? console.log('Success') : console.log('User already exists') 
         console.log(backFromAPI) 
     }
 
-    // async function handleLogin(event) {
-    //     event.preventDefault()
-    //     console.log('this is your event', event)
-    //     const backFromAPI = await loginUser(username, password)
-    //     // backFromAPI ? console.log('Success') : console.log('User already exists') 
-    //     console.log(backFromAPI) 
-    // }
+    async function handleLogin(event) {
+        event.preventDefault()
+        console.log('this is your event', event)
+        const backFromAPI = await loginUser(username, password)
+        localStorage.setItem('token', backFromAPI)
+        // backFromAPI ? console.log('Success') : console.log('User already exists') 
+        console.log(backFromAPI) 
+    }
     return (
         <header>
             <form id = "register" onSubmit = {handleRegister}>
@@ -61,8 +64,50 @@ const [password, setPassword] = useState('');
                 </div>
                 <button type="submit" id = "registerButton">Register</button>
             </form>
+            
 
-            {/* <form id = "login" onSubmit = {handleLogin}>
+            <form id = "login" onSubmit = {handleLogin}>
+                <div className="nameEntry">
+                    <label>Username:</label>
+                    <input type="text" id="inputUsernameLog" name="username"  
+                    value = {username}
+                    onChange={(event) =>{setUsername(event.target.value)
+                    console.log(username);}}
+                    required />
+                </div>
+                <div className="passwordEntry">
+                    <label>Password:</label>
+                    <input type="text" id="inputPasswordLog" name="password" 
+                    value = {password}
+                    onChange={(event) =>{setPassword(event.target.value)
+                    console.log(password);}}    
+                    required />
+                </div>
+        
+                <button type="submit" id = "loginButton">Login</button>
+            </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              {/* <form id = "login" onSubmit = {handleLogin}>
                 <div className="nameEntry">
                     <label>Username:</label>
                     <input type="text" id="inputUsernameLog" name="username"  
@@ -80,7 +125,7 @@ const [password, setPassword] = useState('');
                     required />
                 </div>
                 <button type="submit" id = "loginButton">Login</button>
-            </form> */}
+            </form>   */}
         </header>
     );
 }
